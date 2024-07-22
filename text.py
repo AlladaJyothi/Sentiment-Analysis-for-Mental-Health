cb3e61294d26a7b4b6add980e6115a1d3317dbb2
AlladaJyothi <alladajyothi12@gmail.com>
2024-07-22 21:20:00 +0530

    Update text.py

--- text.py
+++ text.py
@@ -11,10 +11,10 @@ from sklearn.svm import SVC
 import pickle
 import streamlit as st
 
-st.image(r"C:\Users\DELL\Pictures\inno_image.webp")
+st.image("inno_image.webp")
 name=st.title('Sentiment Analysis in Mental Health')
-model=pickle.load(open(r"C:\Users\DELL\Machine Learning\sentiment1.pkl",'rb'))
-bow=pickle.load(open(r"C:\Users\DELL\Machine Learning\sentiment_bow.pkl",'rb'))
+model=pickle.load(open("sentiment1.pkl",'rb'))
+bow=pickle.load(open("sentiment_bow.pkl",'rb'))
 
 test = st.text_input('Enter the statement:')
 st.button("Submit")
